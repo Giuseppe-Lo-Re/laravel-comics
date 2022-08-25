556299c2de94905ffb452f7aef7d56aa6e5d80ab
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-08-25 18:23:10 +0200

    Bonus done

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,43 +12,43 @@
             <!-- Menu -->
             <div class="menu">
                 <ul>
-                    <li class="{{ Route::current()->getName() === 'characters' ? 'current' : ''}}">
+                    <li class="{{ Route::current()->getName() === 'characters' ? 'current' : '' }}">
                         <a href="{{ route('characters') }}">characters</a> 
                     </li>
 
-                    <li class="{{ Route::current()->getName() === 'comics' ? 'current' : ''}}">
+                    <li class="{{ Route::current()->getName() === 'comics' ? 'current' : '' }}">
                         <a href="{{ route('comics') }}">comics</a> 
                     </li>
 
-                    <li class="{{ Route::current()->getName() === 'movies' ? 'current' : ''}}">
+                    <li class="{{ Route::current()->getName() === 'movies' ? 'current' : '' }}">
                         <a href="{{ route('movies') }}">movies</a> 
                     </li>
 
-                    <li class="{{ Route::current()->getName() === 'tv' ? 'current' : ''}}">
+                    <li class="{{ Route::current()->getName() === 'tv' ? 'current' : '' }}">
                         <a href="{{ route('tv') }}">tv</a> 
                     </li>
 
-                    <li class="{{ Route::current()->getName() === 'games' ? 'current' : ''}}">
+                    <li class="{{ Route::current()->getName() === 'games' ? 'current' : '' }}">
                         <a href="{{ route('games') }}">games</a> 
                     </li>
 
-                    <li class="{{ Route::current()->getName() === 'collectibles' ? 'current' : ''}}">
+                    <li class="{{ Route::current()->getName() === 'collectibles' ? 'current' : '' }}">
                         <a href="{{ route('collectibles') }}">collectibles</a> 
                     </li>
 
-                    <li class="{{ Route::current()->getName() === 'videos' ? 'current' : ''}}">
+                    <li class="{{ Route::current()->getName() === 'videos' ? 'current' : '' }}">
                         <a href="{{ route('videos') }}">videos</a> 
                     </li>
 
-                    <li class="{{ Route::current()->getName() === 'fans' ? 'current' : ''}}">
+                    <li class="{{ Route::current()->getName() === 'fans' ? 'current' : '' }}">
                         <a href="{{ route('fans') }}">fans</a>
                     </li>
 
-                    <li class="{{ Route::current()->getName() === 'news' ? 'current' : ''}}">
+                    <li class="{{ Route::current()->getName() === 'news' ? 'current' : '' }}">
                         <a href="{{ route('news') }}">news</a>
                     </li>
 
-                    <li class="{{ Route::current()->getName() === 'shop' ? 'current' : ''}}">
+                    <li class="{{ Route::current()->getName() === 'shop' ? 'current' : '' }}">
                         <a href="{{ route('shop') }}">shop</a>
                     </li>
                 </ul>
